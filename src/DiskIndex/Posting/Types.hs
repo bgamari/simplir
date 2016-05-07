6164e3d829bdca4c82040bed0c2301884d1dbc26
Ben Gamari <ben@smart-cactus.org>
2016-05-07 16:18:04 +0200

    DiskIndex.Postings.Types: More callstacks

--- src/DiskIndex/Posting/Types.hs
+++ src/DiskIndex/Posting/Types.hs
@@ -3,12 +3,15 @@
 
 module DiskIndex.Posting.Types where
 
-import Data.Binary
 import GHC.Generics
+import GHC.Stack
 import Data.Monoid
-import Data.SmallNat
+
+import Data.Binary
 import qualified Data.Vector as V
 import Data.Vector.Binary () -- for instances
+
+import Data.SmallNat
 import qualified Encoded as E
 import Types
 
@@ -16,22 +19,24 @@ newtype DocIdDelta = DocIdDelta SmallNat
                    deriving (Show, Binary, Enum, Bounded, Ord, Eq)
 
 -- | Lift an 'Int' into a 'DocIdDelta'
-toDocIdDelta :: Int -> DocIdDelta
+toDocIdDelta :: HasCallStack => Int -> DocIdDelta
 toDocIdDelta n
   | n' >= minBound && n' < maxBound = DocIdDelta n'
   | otherwise                       = error "toDocIdDelta: Bad delta"
   where n' = fromIntegral n :: SmallNat
+{-# INLINE toDocIdDelta #-}
 
 instance Monoid DocIdDelta where
     mempty = DocIdDelta 0
     DocIdDelta a `mappend` DocIdDelta b = DocIdDelta (a + b)
 
 -- | Take the difference between two 'DocumentId's
-docIdDelta :: DocumentId -> DocumentId -> DocIdDelta
+docIdDelta :: HasCallStack => DocumentId -> DocumentId -> DocIdDelta
 docIdDelta (DocId a) (DocId b)
   | delta < 0  = error "negative DocIdDelta"
   | otherwise  = DocIdDelta $ fromIntegral delta
   where delta = b - a
+{-# INLINE docIdDelta #-}
 
 applyDocIdDelta :: DocumentId -> DocIdDelta -> DocumentId
 applyDocIdDelta (DocId n) (DocIdDelta d) = DocId (n + fromIntegral d)
