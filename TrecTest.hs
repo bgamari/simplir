b828c913ecea21f81e4a475ff74c42238bbeddd4
Ben Gamari <ben@smart-cactus.org>
2016-05-14 21:20:58 +0200

    TrecTest: Chunk indexing

--- TrecTest.hs
+++ TrecTest.hs
@@ -20,6 +20,7 @@ import qualified Data.Vector.Generic as VG
 import qualified Data.Vector.Unboxed as VU
 import           Data.Vector.Algorithms.Intro (sort)
 import qualified Control.Foldl as Foldl
+import           Control.Foldl (FoldM, Fold)
 
 import           Pipes
 import           Pipes.Safe
@@ -35,7 +36,7 @@ import DataSource
 import DiskIndex
 
 dsrcs = map LocalFile [ "data/robust04/docs/ft91.dat.gz"
-                      --, "data/robust04/docs/ft94.dat.gz"
+                      , "data/robust04/docs/ft94.dat.gz"
                       ]
 compression = Just GZip
 
@@ -52,29 +53,34 @@ main = do
         docs =
             mapM_ (trecDocuments' . P.T.decodeUtf8 . decompress compression . produce) dsrcs
 
-    (docIds, postings) <-
-            runSafeT
-         $  consumePostings
-         $  docs
-        >-> cat'                                          @TREC.Document
-        >-> P.P.map (\d -> (DocName $ BS.S.toShort $ T.E.encodeUtf8 $ TREC.docNo d, TREC.docText d))
-        >-> cat'                                          @(DocumentName, T.Text)
-        >-> P.P.map (fmap tokeniseWithPositions)
-        >-> cat'                                          @(DocumentName, [(Term, Position)])
-        >-> P.P.map (fmap normTerms)
-        >-> cat'                                          @(DocumentName, [(Term, Position)])
-        >-> zipWithList [DocId 0..]
-        >-> cat'                                          @(DocumentId, (DocumentName, [(Term, Position)]))
-        >-> P.P.map (\(docId, (docName, postings)) ->
-                      ((docId, docName),
-                        toPostings docId
-                        $ M.assocs
-                        $ foldTokens accumPositions postings))
-        >-> cat'                                          @((DocumentId, DocumentName), TermPostings (VU.Vector Position))
+    let chunkIndexes :: Producer (FragmentIndex (VU.Vector Position)) (SafeT IO) ()
+        chunkIndexes =
+                consumePostings 10000
+             $  docs
+            >-> cat'                                          @TREC.Document
+            >-> P.P.map (\d -> (DocName $ BS.S.toShort $ T.E.encodeUtf8 $ TREC.docNo d, TREC.docText d))
+            >-> cat'                                          @(DocumentName, T.Text)
+            >-> P.P.map (fmap tokeniseWithPositions)
+            >-> cat'                                          @(DocumentName, [(Term, Position)])
+            >-> P.P.map (fmap normTerms)
+            >-> cat'                                          @(DocumentName, [(Term, Position)])
+            >-> zipWithList [DocId 0..]
+            >-> cat'                                          @(DocumentId, (DocumentName, [(Term, Position)]))
+            >-> P.P.map (\(docId, (docName, postings)) ->
+                          ((docId, docName),
+                            toPostings docId
+                            $ M.assocs
+                            $ foldTokens accumPositions postings))
+            >-> cat'                                          @((DocumentId, DocumentName), TermPostings (VU.Vector Position))
 
-    DiskIndex.fromDocuments "index" (M.toList docIds) (fmap V.toList postings)
+    runSafeT $ runEffect $ for (chunkIndexes >-> zipWithList [0..]) $ \(n, (docIds, postings)) -> do
+        liftIO $ print (n, M.size docIds)
+        liftIO $ DiskIndex.fromDocuments ("index-"++show n) (M.toList docIds) (fmap V.toList postings)
 
 type SavedPostings p = M.Map Term (V.Vector (Posting p))
+type FragmentIndex p =
+    ( M.Map DocumentId (DocumentName, DocumentLength)
+    , SavedPostings p)
 
 zipWithList :: Monad m => [i] -> Pipe a (i,a) m r
 zipWithList = go
@@ -85,18 +91,40 @@ zipWithList = go
         yield (i, x)
         go is
 
-consumePostings :: (Monad m, Ord p)
-                => Producer ((DocumentId, DocumentName), TermPostings p) m ()
-                -> m ( M.Map DocumentId (DocumentName, DocumentLength)
-                     , SavedPostings p)
-consumePostings =
-    foldProducer $ (,) <$> docMeta
-                       <*> lmap snd postings
+foldChunks :: Monad m => Int -> FoldM m a b -> Producer a m () -> Producer b m ()
+foldChunks chunkSize (Foldl.FoldM step initial extract) = start
   where
-    docMeta  = Foldl.generalize
-               $ lmap (\((docId, docName), postings) -> M.singleton docId (docName, DocLength $ sum $ fmap length postings))
+    start prod = do
+        acc <- lift initial
+        go chunkSize acc prod
+
+    go 0 acc prod = do
+        lift (extract acc) >>= yield
+        start prod
+    go n acc prod = do
+        mx <- lift $ next prod
+        case mx of
+          Right (x, prod') -> do
+              acc' <- lift $ step acc x
+              go (n-1 :: Int) acc' prod'
+          Left r -> lift (extract acc) >>= yield
+
+consumePostings :: (Monad m, Ord p)
+                => Int
+                -> Producer ((DocumentId, DocumentName), TermPostings p) m ()
+                -> Producer (FragmentIndex p) m ()
+consumePostings chunkSize =
+    foldChunks chunkSize (Foldl.generalize consumePostings')
+
+consumePostings' :: (Ord p)
+                 => Fold ((DocumentId, DocumentName), TermPostings p)
+                         (FragmentIndex p)
+consumePostings' =
+    (,) <$> docMeta <*> lmap snd postings
+  where
+    docMeta  = lmap (\((docId, docName), postings) -> M.singleton docId (docName, DocLength $ sum $ fmap length postings))
                       Foldl.mconcat
-    postings = Foldl.generalize $ fmap (M.map $ VG.modify sort . fromFoldable) foldPostings
+    postings = fmap (M.map $ VG.modify sort . fromFoldable) foldPostings
 
 fromFoldable :: (Foldable f, VG.Vector v a)
              => f a -> v a
