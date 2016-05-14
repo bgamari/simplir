faff6e892a835333084fb0355ac2b302d8d9ad01
Ben Gamari <ben@smart-cactus.org>
2016-05-14 13:03:14 +0200

    Wibbles

--- src/TopK.hs
+++ src/TopK.hs
@@ -6,6 +6,7 @@ import qualified Data.Heap as H
 import Data.Ord
 
 topK :: Ord a => Int -> Fold.Fold a [a]
-topK k = Fold.Fold step H.empty (map (\(Down x) -> x) . toList)
+topK k = Fold.Fold step H.empty (map unDown . toList)
   where
     step acc x = H.take k $ H.insert (Down x) acc
+    unDown (Down x) = x
