964a585b5ad79266ee0710287d2c68e4a9ce6fab
Ben Gamari <ben@smart-cactus.org>
2016-06-04 17:48:30 +0200

    TrecStreaming: Multiple concurrent queries

--- simplir-trec-streaming/Search.hs
+++ simplir-trec-streaming/Search.hs
@@ -9,13 +9,14 @@
 
 import Control.Monad.State.Strict hiding ((>=>))
 import Data.Bifunctor
+import Data.Maybe
 import Data.Monoid
 import Data.Profunctor
 import Data.Tuple
 import Data.Char
-import Numeric.Log
 
 import Data.Binary
+import Numeric.Log
 import qualified Data.ByteString.Lazy.Char8 as BS.L
 import qualified Data.Map.Strict as M
 import qualified Data.Set as S
@@ -44,31 +45,34 @@ type QueryId = String
 scoreMode :: Parser (IO ())
 scoreMode =
     score
-      <$> option str (metavar "QUERY_ID" <> long "qid" <> short 'i' <> value "1")
-      <*> queryTerms
+      <$> optQueryFile
       <*> option auto (metavar "N" <> long "count" <> short 'n' <> value 10)
       <*> some (argument (LocalFile <$> str) (metavar "FILE" <> help "TREC input file"))
-  where
-    queryTerms :: Parser (M.Map Term Int)
-    queryTerms = M.fromListWith (+) . flip zip (repeat 1)
-                 <$> some (option (Term.fromString <$> str)
-                                  (metavar "TERMS" <> long "query" <> short 'q'))
 
 corpusStatsMode :: Parser (IO ())
 corpusStatsMode =
     corpusStats
-      <$> queryTerms
+      <$> optQueryFile
       <*> some (argument (LocalFile <$> str) (metavar "FILE" <> help "TREC input file"))
-  where
-    queryTerms :: Parser (S.Set Term)
-    queryTerms = S.fromList
-                 <$> some (option (Term.fromString <$> str)
-                                  (metavar "TERMS" <> long "query" <> short 'q'))
 
 modes :: Parser (IO ())
 modes = subparser
     $  command "score" (info scoreMode fullDesc)
     <> command "corpus-stats" (info corpusStatsMode fullDesc)
+
+type QueryFile = FilePath
+
+optQueryFile :: Parser QueryFile
+optQueryFile =
+    option str (metavar "FILE" <> long "query" <> short 'q' <> help "query file")
+
+readQueries :: QueryFile -> IO (M.Map QueryId [Term])
+readQueries fname = M.unions . mapMaybe parse . lines <$> readFile fname
+  where
+    parse "" = Nothing
+    parse line
+      | (qid, body) <- span (/= '\t') line
+      = Just $ M.singleton qid (map Term.fromString $ words body)
 
 compression = Just Lzma
 
@@ -77,8 +81,10 @@ main = do
     mode <- execParser $ info (helper <*> modes) fullDesc
     mode
 
-corpusStats :: S.Set Term -> [DataLocation] -> IO ()
-corpusStats queryTerms docs = do
+corpusStats :: QueryFile -> [DataLocation] -> IO ()
+corpusStats queryFile docs = do
+    queries <- readQueries queryFile
+    let queryTerms = foldMap S.fromList queries
     runSafeT $ do
         idx@(termFreqs, collLength) <-
                 foldProducer (Foldl.generalize indexPostings)
@@ -108,33 +114,48 @@ indexPostings =
         $ lmap (\term -> M.singleton term (TermFreq 1))
         $ mconcatMaps
 
-score :: QueryId -> M.Map Term Int -> Int -> [DataLocation] -> IO ()
-score qid queryTerms resultCount docs = do
-    -- background statistics
+score :: QueryFile -> Int -> [DataLocation] -> IO ()
+score queryFile resultCount docs = do
+    queries <- readQueries queryFile
+
+    -- load background statistics
     (termFreqs, collLength) <- decode <$> BS.L.readFile "background"
         :: IO CorpusStats
     let getTermFreq term = maybe mempty id $ M.lookup term termFreqs
         smoothing = Dirichlet 2500 ((\n -> (n + 0.5) / (realToFrac collLength + 1)) . getTermFrequency . getTermFreq)
 
-    let scoreTerms :: [(Term, Position)] -> Score
-        scoreTerms terms =
-            let docLength = DocLength $ length terms
-                terms' = map (\(term,_) -> (term, 1)) terms
-            in queryLikelihood smoothing (M.assocs queryTerms) docLength terms'
+
+    let queryFolds :: Foldl.Fold (DocumentName, [Term])
+                                 (M.Map QueryId [(Score, DocumentName)])
+        queryFolds = sequenceA $ fmap queryFold queries
+
+        queryFold :: [Term] -> Foldl.Fold (DocumentName, [Term]) [(Score, DocumentName)]
+        queryFold queryTerms =
+            let queryTerms' :: M.Map Term Int
+                queryTerms' = M.fromListWith (+) $ zip queryTerms (repeat 1)
+                scoreTerms :: [Term] -> Score
+                scoreTerms terms =
+                    let docLength = DocLength $ length terms
+                        terms' = zip terms (repeat 1)
+                    in queryLikelihood smoothing (M.assocs queryTerms') docLength terms'
+            in lmap (swap . second scoreTerms) $ topK resultCount
 
     runSafeT $ do
         results <-
-                foldProducer (Foldl.generalize $ topK resultCount)
+                foldProducer (Foldl.generalize queryFolds)
              $  streamingDocuments docs
             >-> normalizationPipeline
             >-> cat'                                          @(DocumentName, [(Term, Position)])
-            >-> P.P.filter (any (`M.member` queryTerms) . map fst . snd)
-            >-> P.P.map (swap . second scoreTerms)
-            >-> cat'                                          @(Score, DocumentName)
+            -- >-> P.P.filter (any (`S.member` allQueryTerms) . map fst . snd)
+            >-> P.P.map (second $ map fst)
+            >-> cat'                                          @(DocumentName, [Term])
 
-        let toRunFile rank (Exp score, DocName docName) = unwords
-                [ qid, "Q0", Utf8.toString docName, show rank, show score, "simplir" ]
-        liftIO $ putStrLn $ unlines $ zipWith toRunFile [1..] results
+        liftIO $ putStrLn $ unlines
+            [ unwords [ qid, "Q0", Utf8.toString docName, show rank, show score, "simplir" ]
+            | (qid, scores) <- M.toList results
+            , (rank, (Exp score, DocName docName)) <- zip [1..] scores
+            ]
+        return ()
 
 type ArchiveName = T.Text
 
