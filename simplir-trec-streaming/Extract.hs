1a5f413e45c8ba061e7ea73b85e03e888c8a91ca
Ben Gamari <ben@smart-cactus.org>
2016-06-24 12:49:28 +0200

    Add extract-kba-documents

---
+++ simplir-trec-streaming/Extract.hs
@@ -0,0 +1,67 @@
+{-# LANGUAGE RecordWildCards #-}
+
+import qualified Data.Map as M
+import qualified Data.Set as S
+import qualified Data.Text as T
+import qualified Data.ByteString as BS
+import qualified Data.ByteString.Lazy as BS.L
+import Data.Aeson as Aeson
+import Options.Applicative
+import Pipes
+import Pipes.Safe
+import qualified Pinch
+import qualified Pinch.Protocol as Pinch
+import qualified Pinch.Internal.Builder as Pinch
+import qualified Pipes.Prelude as P.P
+import qualified Control.Foldl as Foldl
+import System.FilePath
+
+import Control.Foldl.Map
+import qualified Data.SmallUtf8 as Utf8
+import SimplIR.TrecStreaming as Kba
+import SimplIR.Utils
+import SimplIR.DataSource
+import SimplIR.Types
+import Types
+import ReadKba
+
+main :: IO ()
+main = do
+    let args = some $ argument str (help "ranking file")
+    fnames <- execParser $ info (helper <*> args) mempty
+
+    let getDocs :: ScoredDocument -> M.Map DataSource (S.Set DocumentName)
+        getDocs (ScoredDocument{scoredDocumentInfo=DocInfo{..}})
+          | Just dsrc <- parseDataSource docArchive =
+                M.singleton dsrc (S.singleton docName)
+          | otherwise = mempty
+
+    neededDocs <- foldProducer (Foldl.generalize mconcatMaps)
+       $  each fnames
+      >-> P.P.mapM readRanking
+      >-> P.P.mapFoldable (map $ map getDocs . take 100 . rankingResults)
+      >-> P.P.concat
+
+    mapM_ (uncurry dumpDocuments) $ M.assocs neededDocs
+
+dumpDocuments :: DataSource -> S.Set DocumentName -> IO ()
+dumpDocuments dsrc docs = do
+    takenDocs <- takeDocuments docs . BS.L.toStrict <$> runSafeT (readKbaFile dsrc)
+    let outPath = T.unpack (getFileName $ dsrcLocation dsrc) <.> "extracted"
+    BS.writeFile outPath
+        $ Pinch.runBuilder
+        $ foldMap (Pinch.serializeValue Pinch.binaryProtocol) takenDocs
+
+takeDocuments :: S.Set DocumentName -> BS.ByteString -> [Pinch.Value Pinch.TStruct]
+takeDocuments docs = go
+  where
+    go bs
+      | docName `S.member` docs = val : go bs'
+      | otherwise = go bs'
+      where
+        (bs', val) = either error id $ Pinch.deserializeValue' Pinch.binaryProtocol bs
+        item = either error id $ Pinch.runParser (parseStreamItem val)
+        docName = DocName (Utf8.fromText $ Kba.getDocumentId $ Kba.documentId item)
+
+readRanking :: FilePath -> IO [Ranking]
+readRanking fname = either fail id . Aeson.eitherDecode <$> BS.L.readFile fname
