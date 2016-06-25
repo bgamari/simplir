e13e6971ced326c2f577eca02ac904a3ef3af609
Ben Gamari <ben@smart-cactus.org>
2016-06-25 22:50:06 +0200

    ListDocuments: Refactor

--- simplir-trec-streaming/ListDocuments.hs
+++ simplir-trec-streaming/ListDocuments.hs
@@ -1,10 +1,13 @@
 import SimplIR.TrecStreaming
+import SimplIR.DataSource
 import Control.Monad (forM_)
+import Pipes.Safe
 import qualified Data.ByteString as BS
 import qualified Data.ByteString.Lazy as BSL
 import qualified Data.Text as T
 import Options.Applicative
 import Codec.Compression.Lzma
+import ReadKba
 
 args :: Parser [FilePath]
 args = some $ argument str (help "Thrift-encoded corpus fragment")
@@ -14,6 +17,7 @@ main = do
     fnames <- execParser $ info (helper <*> args) mempty
     forM_ fnames $ \fname -> do
         putStrLn fname
-        items <- readItems . BSL.toStrict . decompress <$> BSL.readFile fname
+        Just dsrc <- return $ parseDataSource $ T.pack fname
+        items <- readItems . BSL.toStrict <$> runSafeT (readKbaFile dsrc)
         putStrLn $ unlines $ map (T.unpack . getDocumentId . documentId) items
 
