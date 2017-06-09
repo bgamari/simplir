586930892ac8580d7ce43709a6a690600da1acb6
Ben Gamari <ben@smart-cactus.org>
2017-06-08 22:57:28 -0400

    BM25: Refactoring, use Lucene's IDF, filter on query terms

--- src/SimplIR/RetrievalModels/BM25.hs
+++ src/SimplIR/RetrievalModels/BM25.hs
@@ -7,6 +7,7 @@ import Data.Maybe
 import Data.Hashable
 import Numeric.Log hiding (sum)
 import qualified Data.HashMap.Strict as HM
+import qualified Data.HashSet as HS
 import SimplIR.RetrievalModels.CorpusStats
 
 type Score = Log Double
@@ -20,21 +21,32 @@ sensibleParams :: BM25Params
 sensibleParams = BM25Params 1.2 0.75
 
 bm25 :: (Eq term, Hashable term)
-     => BM25Params -> CorpusStats term -> DocLength -> term -> TermFreq -> Score
-bm25 params CorpusStats{..} docLen term tf =
-    bm25' params corpusDocCount corpusTokenCount docLen termStats tf
+     => BM25Params -> CorpusStats term
+     -> HS.HashSet term -> DocLength -> HM.HashMap term TermFreq -> Score
+bm25 params stats queryTerms docLen terms =
+    sum $ map (uncurry $ bm25Term params stats docLen)
+    $ filter (\(term, _) -> term `HS.member` queryTerms)
+    $ HM.toList terms
+
+bm25Term :: (Eq term, Hashable term)
+         => BM25Params -> CorpusStats term
+         -> DocLength -> term -> TermFreq -> Score
+bm25Term params CorpusStats{..} docLen term tf =
+    bm25Term' params corpusDocCount corpusTokenCount docLen termStats tf
   where
     termStats = fromMaybe mempty $ HM.lookup term corpusTerms
 {-# INLINEABLE bm25 #-}
 
-bm25' :: BM25Params -> CorpusDocCount -> CorpusTokenCount -> DocLength -> TermStats -> TermFreq -> Score
-bm25' BM25Params{..} docCount tokCount docLen TermStats{..} tf =
-    idf * tf' * (k1 + 1) / (tf'  + k1 * (1 - b + b * realToFrac docLen / avgDocLen))
+bm25Term' :: BM25Params -> CorpusDocCount -> CorpusTokenCount
+          -> DocLength -> TermStats -> TermFreq -> Score
+bm25Term' BM25Params{..} docCount tokCount docLen TermStats{..} tf =
+    idf * tf' * (k1 + 1) / (tf' + k1 * (1 - b + b * realToFrac docLen / avgDocLen))
   where
     avgDocLen = realToFrac tokCount / realToFrac docCount
     tf' = realToFrac tf
     docFreq = realToFrac documentFrequency
-    idf = log $ realToFrac num / realToFrac denom
+    -- uses Lucene's BM-25 IDF
+    idf = log $ 1 + realToFrac num / realToFrac denom
       where
         num, denom :: Double
         num = realToFrac docCount - docFreq + 0.5
