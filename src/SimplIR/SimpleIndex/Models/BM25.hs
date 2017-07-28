249e3ddabe52de3a8ebb4022b700442e75e9a67a
Laura Dietz <dietz@cs.umass.edu>
2017-07-27 22:03:30 -0400

    BM25 reexport params

--- src/SimplIR/SimpleIndex/Models/BM25.hs
+++ src/SimplIR/SimpleIndex/Models/BM25.hs
@@ -1,6 +1,7 @@
 module SimplIR.SimpleIndex.Models.BM25
     ( BM25.BM25Params(..)
     , bm25
+    , BM25.sensibleParams
     ) where
 
 import Data.Hashable
@@ -10,7 +11,8 @@ import qualified Data.HashMap.Strict as HM
 import SimplIR.SimpleIndex
 import qualified SimplIR.RetrievalModels.BM25 as BM25
 
-bm25 :: (Eq term, Hashable term) => BM25.BM25Params
+bm25 :: (Eq term, Hashable term)
+     => BM25.BM25Params
      -> RetrievalModel term doc Int
 bm25 params stats = \queryTerms ->
     let queryTerms' = HS.fromList queryTerms
