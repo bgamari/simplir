7b6941f078febcf30c88efff71c45d02999acf08
Ben Gamari <ben@smart-cactus.org>
2018-01-04 12:12:20 -0500

    DiskIndex: Use tree reduction during index merging

--- src/Control/Concurrent/Map.hs
+++ src/Control/Concurrent/Map.hs
@@ -1,17 +1,27 @@
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+
 module Control.Concurrent.Map (mapConcurrentlyL) where
 
-import Control.Concurrent.Async
+import Control.Monad.Trans.Control
+import Control.Monad.IO.Class
+import Control.Monad.Catch
+import Control.Concurrent.Async.Lifted
 import Control.Concurrent.STM
 import Control.Concurrent.STM.TSem
-import Control.Exception
 
 -- | Map concurrently with a limit to the number of concurrent workers.
-mapConcurrentlyL :: Traversable f => Int -> (a -> IO b) -> f a -> IO (f b)
+mapConcurrentlyL :: forall m f a b.
+                    (MonadBaseControl IO m, MonadIO m, MonadMask m, Traversable f)
+                 => Int -> (a -> m b) -> f a -> m (f b)
 mapConcurrentlyL n f xs = do
-    sem <- atomically $ newTSem n
-    let withSem :: IO a -> IO a
-        withSem g =
-            bracket (atomically $ waitTSem sem)
-                    (\_ -> atomically $ signalTSem sem)
-                    (\_ -> g)
-    mapConcurrently (withSem . f) xs
+    sem <- liftIO $ atomically $ newTSem n
+    mapConcurrently (withTSem sem . f) xs
+{-# INLINEABLE mapConcurrentlyL #-}
+
+withTSem :: (MonadMask m, MonadIO m) => TSem -> m a -> m a
+withTSem sem g =
+    bracket (liftIO $ atomically $ waitTSem sem)
+            (\_ -> liftIO $ atomically $ signalTSem sem)
+            (\_ -> g)
+{-# INLINEABLE withTSem #-}
--- src/SimplIR/DiskIndex/Build.hs
+++ src/SimplIR/DiskIndex/Build.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE BangPatterns #-}
 
@@ -6,10 +7,14 @@ module SimplIR.DiskIndex.Build
     ( buildIndex
     ) where
 
+import Control.Monad.Trans.Control
+import Control.Concurrent
+import Control.Concurrent.Map
 import Control.Monad.IO.Class
 import Data.Profunctor
-import System.Directory (createDirectoryIfMissing, removeDirectoryRecursive)
+import System.Directory
 import System.IO.Temp
+import Data.List.Split
 
 import qualified Data.Map.Strict as M
 import qualified Control.Foldl as Foldl
@@ -21,27 +26,56 @@ import SimplIR.Types
 import Control.Foldl.Map
 import SimplIR.Utils
 
-buildIndex :: forall term m doc p. (MonadSafe m, Binary term, Ord term, Binary doc, Binary p)
+buildIndex :: forall term m doc p. (MonadBaseControl IO m, MonadSafe m, Binary term, Ord term, Binary doc, Binary p)
            => Int       -- ^ How many documents to include in an index chunk?
            -> FilePath  -- ^ Final index path
            -> Foldl.FoldM m (doc, M.Map term p) (DiskIdx.OnDiskIndex term doc p)
 buildIndex chunkSize outputPath =
-    postmapM' (mergeIndexChunks outputPath)
+    postmapM' moveResult
+    $ postmapM' (treeReduce 256 mergeIndexChunks)
     $ foldChunksOf chunkSize (Foldl.generalize collectIndex) writeIndexChunks
+  where
+    moveResult :: (DiskIdx.OnDiskIndex term doc p, ReleaseKey)
+               -> m (DiskIdx.OnDiskIndex term doc p)
+    moveResult (DiskIdx.OnDiskIndex path, key) = do
+        liftIO $ renameDirectory path outputPath
+        release key  -- will fail
+        return (DiskIdx.OnDiskIndex outputPath)
 {-# INLINEABLE buildIndex #-}
+
+treeReduce :: (MonadBaseControl IO m, MonadSafe m)
+           => Int   -- ^ maximum reduction width
+           -> ([a] -> m a)
+           -> [a]
+           -> m a
+treeReduce _ _ [x] = return x
+treeReduce width f xs  = do
+    let reductions = chunksOf width xs
+    caps <- liftIO getNumCapabilities
+    reduced <- mapConcurrentlyL caps f reductions
+    treeReduce width f reduced
+{-# INLINEABLE treeReduce #-}
 
 -- | Perform the final merge of a set of index chunks.
 mergeIndexChunks :: (MonadSafe m, Binary term, Ord term, Binary doc, Binary p)
-            => FilePath
-            -> [(DiskIdx.OnDiskIndex term doc p, ReleaseKey)]
-            -> m (DiskIdx.OnDiskIndex term doc p)
-mergeIndexChunks outputPath chunks = do
+            => [(DiskIdx.OnDiskIndex term doc p, ReleaseKey)]
+            -> m (DiskIdx.OnDiskIndex term doc p, ReleaseKey)
+mergeIndexChunks chunks = do
     let (chunkFiles, chunkKeys) = unzip chunks
     idxs <- liftIO $ mapM DiskIdx.openOnDiskIndex chunkFiles
-    liftIO $ DiskIdx.merge outputPath idxs
+    (path, key) <- newTempDir
+    liftIO $ DiskIdx.merge path idxs
     mapM_ release chunkKeys
-    return (DiskIdx.OnDiskIndex outputPath)
+    return (DiskIdx.OnDiskIndex path, key)
 {-# INLINEABLE mergeIndexChunks #-}
+
+newTempDir :: (MonadSafe m)
+           => m (FilePath, ReleaseKey)
+newTempDir = do
+    liftIO $ createDirectoryIfMissing True ".build-index"
+    path <- liftIO $ createTempDirectory ".build-index" "part.index"
+    key <- register $ liftIO $ removeDirectoryRecursive path
+    return (path, key)
 
 -- | Write a set of index chunks.
 writeIndexChunks :: forall term doc p m. (MonadSafe m, Binary term, Ord term, Binary doc, Binary p)
@@ -54,9 +88,7 @@ writeIndexChunks =
     chunkToIndex :: ([(DocumentId, doc)], M.Map term [Posting p])
                  -> m (DiskIdx.OnDiskIndex term doc p, ReleaseKey)
     chunkToIndex (docIdx, postingIdx) = do
-        liftIO $ createDirectoryIfMissing True ".build-index"
-        path <- liftIO $ createTempDirectory ".build-index" "part.index"
-        key <- register $ liftIO $ removeDirectoryRecursive path
+        (path, key) <- newTempDir
         liftIO $ DiskIdx.fromDocuments path docIdx postingIdx
         return (DiskIdx.OnDiskIndex path, key)
 {-# INLINEABLE writeIndexChunks #-}
