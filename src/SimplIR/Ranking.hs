96b983969fd140c4491dc72fd7618f0e9f9f7895
Ben Gamari <ben@smart-cactus.org>
2018-01-29 01:40:59 -0500

    Ranking: Add Bifunctor instance

--- src/SimplIR/Ranking.hs
+++ src/SimplIR/Ranking.hs
@@ -11,10 +11,14 @@ module SimplIR.Ranking
 
 import Data.List
 import Data.Ord
+import Data.Bifunctor
 
 -- | A sorted list of items and their scores
 newtype Ranking score a = Ranking [(score, a)]
                         deriving (Show, Read, Eq, Functor, Foldable, Traversable)
+
+instance Bifunctor Ranking where
+    bimap f g (Ranking xs) = Ranking $ map (\(x,y) -> (f x, g y)) xs
 
 fromList :: Ord score => [(score, a)] -> Ranking score a
 fromList = Ranking . sortBy (comparing $ Down . fst)
