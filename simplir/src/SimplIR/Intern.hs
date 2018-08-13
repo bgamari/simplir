b05ace0c482560b17ccdf3c9e9710f327af369f5
Laura Dietz <dietz@cs.umass.edu>
2018-08-13 13:28:20 -0400

    Make InternM a monad transformer

--- simplir/src/SimplIR/Intern.hs
+++ simplir/src/SimplIR/Intern.hs
@@ -1,5 +1,8 @@
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE UndecidableInstances #-}
 
 module SimplIR.Intern
     ( InternM
@@ -13,6 +16,8 @@ import Data.Tuple
 import Data.Hashable
 import Control.Lens
 import Control.Monad.Trans.State.Strict
+import Control.Monad.Trans.Class
+import Control.Monad.IO.Class
 import qualified Data.HashMap.Strict as HM
 
 -- | The interning table
@@ -27,17 +32,19 @@ insert b (Table m)
   | Just b' <- HM.lookup b m = (Table m, b')
   | otherwise                = (Table $ HM.insert b b m, b)
 
-newtype InternM b a = InternM (State (Table b) a)
-                    deriving (Functor, Applicative)
+newtype InternM b m a = InternM (StateT (Table b) m a)
+                      deriving (Functor, Applicative, Monad, MonadIO, MonadTrans)
 
-runInternM' :: InternM b a -> (Table b, a)
-runInternM' (InternM m) = swap $ runState m emptyTable
+runInternM' :: forall b m a. Functor m => InternM b m a -> m (Table b, a)
+runInternM' (InternM m) = swap <$> runStateT m emptyTable
 
-runInternM :: InternM b a -> a
-runInternM = snd . runInternM'
+runInternM :: forall b m a. Functor m => InternM b m a -> m a
+runInternM = fmap snd . runInternM'
 
-internAll :: (Eq b, Hashable b) => Traversal' a b -> a -> InternM b a
+internAll :: (Monad m, Eq b, Hashable b)
+          => Optical (->) (->) (InternM b m) a a b b  -- ^ e.g. @Traversal a a b b@
+          -> a -> InternM b m a
 internAll t = traverseOf t intern
 
-intern :: (Eq a, Hashable a) => a -> InternM a a
+intern :: (Monad m, Eq a, Hashable a) => a -> InternM a m a
 intern x = InternM $ state $ swap . insert x
