6693dadd0dc00d78c0cb1beac07201c975eeece2
Ben Gamari <ben@smart-cactus.org>
2018-12-23 21:10:09 -0500

    Bump trec_eval

--- trec-eval.nix
+++ trec-eval.nix
@@ -3,19 +3,11 @@
 stdenv.mkDerivation {
   name = "trec-eval";
   src = fetchFromGitHub {
-    owner = "bgamari";
-    repo = "trec_eval";
-    rev = "master";
-    sha256 = null;
-  };
-  /* Pending #16
-  src = fetchFromGitHub {
     owner = "usnistgov";
     repo = "trec_eval";
-    rev = "v9.0.5";
-    sha256 = null;
+    rev = "a5211566d0c9e2ec337bacf327b9350ab5b3edde";
+    sha256 = "10v9hg9919maz4x088jzv9vb4mhv4qij85kmz3i2vgk5aphnwzp2";
   };
-  */
   installPhase = ''
     mkdir -p $out/bin
     cp trec_eval $out/bin
