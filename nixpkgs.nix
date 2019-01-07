ef0a381df4e307bb0c3516dea82107178980d1a7
Ben Gamari <ben@smart-cactus.org>
2019-01-07 00:34:26 -0500

    Rework nixpkgs.nix

--- nixpkgs.nix
+++ nixpkgs.nix
@@ -1,11 +1,8 @@
 let
-  nixpkgs = import <nixpkgs> {};
-  src = 
-    nixpkgs.fetchFromGitHub {
-      owner = "nixos";
-      repo = "nixpkgs";
-      rev = "9592e380c5ce1b0a437186feead82878fed77efc";
-      sha256 = "13x8szfy3l15m0iwl59bnn52cwhbkb2fkd91nn85yd6brh2dg8jb";
-    };
-in
-  import src
+  rev = "9592e380c5ce1b0a437186feead82878fed77efc";
+  sha256 = "13x8szfy3l15m0iwl59bnn52cwhbkb2fkd91nn85yd6brh2dg8jb";
+  tarball = builtins.fetchTarball {
+    url = "https://github.com/nixos/nixpkgs/archive/${rev}.tar.gz";
+    inherit sha256;
+  };
+in import tarball
